0303ba94a72ad811a118c4fcd78379f9fa9cadb0
Is <isaiaslflores@gmail.com>
2025-10-28 19:28:39 -0500

    dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Home from './app/Home'
+import Resume from './app/Resume'
+import Contact from './app/Contact'
+import LayoutMain from './app/LayoutMain'
+import { ThemeProvider } from './components/theme-provider'
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <ThemeProvider defaultTheme="dark" storageKey="IsDev-ui-theme">
+        <Router>
+          <Routes>
+            <Route path="/" element={<LayoutMain />}>
+              <Route index element={<Home />} />
+              <Route path="/resume" element={<Resume />} />
+              <Route path="/contact" element={<Contact />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   )
 }
