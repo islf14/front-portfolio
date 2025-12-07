5ad254b9818c242ab7c2310a71b950f84c9739ce
Is <isaiaslflores@gmail.com>
2025-12-07 11:42:06 -0500

    client directory

--- client/src/app/Home.tsx
+++ client/src/app/Home.tsx
@@ -1,6 +1,6 @@
-import work1 from '../assets/work/work1.png'
 import shortener from '../assets/work/shortener.png'
-import work2 from '../assets/work/work2.png'
+import chat from '../assets/work/chat.png'
+import business from '../assets/work/business.png'
 import HtmlIcon from '@/components/svg/HtmlIcon'
 import CssIcon from '@/components/svg/CssIcon'
 import TsIcon from '@/components/svg/TsIcon'
@@ -12,6 +12,12 @@ import GhIcon from '@/components/svg/GhIcon'
 import ProjectCard, { type PjCardProps } from './ProjectCard'
 import MongoIcon from '@/components/svg/MongoIcon'
 import JsIcon from '@/components/svg/JsIcon'
+import SqliteIcon from '@/components/svg/SqliteIcon'
+import NestIcon from '@/components/svg/NestIcon'
+import ReactIcon from '@/components/svg/ReactIcon'
+import ReactRouterIcon from '@/components/svg/ReactRouterIcon'
+import TailwindIcon from '@/components/svg/TailwindIcon'
+import ViteIcon from '@/components/svg/ViteIcon'
 
 const priDescription =
   'I have practical experience in MERN stack, with mastery in Express JS, NestJS and also Laravel, with the intention of providing my skills in web applications, mobile applications, databases and API development to contribute to innovative projects.'
@@ -25,14 +31,14 @@ const projects: PjCardProps[] = [
     technologies: [
       { name: 'NodeJs', icon: <NodeIcon /> },
       { name: 'TypeScript', icon: <TsIcon /> },
-      { name: 'Express Js', icon: <ExIcon /> },
+      { name: 'Express.JS', icon: <ExIcon /> },
       { name: 'MongoDB', icon: <MongoIcon /> },
       { name: 'Html', icon: <HtmlIcon /> },
       { name: 'Css', icon: <CssIcon /> },
       { name: 'JavaScript', icon: <JsIcon /> }
     ],
     links: [
-      { url: 'https://is-url.vercel.app/', icon: <LinkIcon /> },
+      { url: 'https://is-url.vercel.app', icon: <LinkIcon /> },
       {
         url: 'https://github.com/islf14/url-shortening-service',
         icon: <GhIcon />
@@ -40,52 +46,42 @@ const projects: PjCardProps[] = [
     ]
   },
   {
-    title: 'Back',
-    image: work2,
+    title: 'Chat',
+    image: chat,
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, provident officia? Corporis et deserunt detis in ea soluta facilis ullam.',
+      'This web application creates a chat room where anyone can join and send messages. It is not possible for two users to have the same name. When the last user logs out, the chat closes and users are reset. Then, the new user logs in and creates a new chat.',
     technologies: [
-      { name: 'TypeScript', icon: <TsIcon /> },
       { name: 'NodeJs', icon: <NodeIcon /> },
-      { name: 'Express', icon: <ExIcon /> },
-      { name: 'PostgreSQL', icon: <PgIcon /> }
-    ],
-    links: [
-      { url: 'github.com', icon: <LinkIcon /> },
-      { url: 'github.com', icon: <GhIcon /> }
-    ]
-  },
-  {
-    title: 'Project in NestJS',
-    image: work1,
-    description: 'Lorem ipsum d ?cilis ullam.',
-    technologies: [
-      { name: 'Html', icon: <HtmlIcon /> },
       { name: 'TypeScript', icon: <TsIcon /> },
-      { name: 'Express', icon: <ExIcon /> },
-      { name: 'PostgreSQL', icon: <PgIcon /> }
-    ],
-    links: [
-      { url: 'github.com', icon: <LinkIcon /> },
-      { url: 'github.com', icon: <GhIcon /> }
-    ]
-  },
-  {
-    title: 'URL shortener',
-    image: work1,
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, provident officia? Corporis et deserunt deleniti nemo in aut! Est aperiam et sequi minima architecto debitis in ea soluta facilis ullam.',
-    technologies: [
+      { name: 'ExpressJS', icon: <ExIcon /> },
+      { name: 'SQLite', icon: <SqliteIcon /> },
       { name: 'Html', icon: <HtmlIcon /> },
       { name: 'Css', icon: <CssIcon /> },
-      { name: 'TypeScript', icon: <TsIcon /> },
-      { name: 'NodeJs', icon: <NodeIcon /> },
-      { name: 'Express', icon: <ExIcon /> },
-      { name: 'PostgreSQL', icon: <PgIcon /> }
+      { name: 'JavaScript', icon: <JsIcon /> }
     ],
     links: [
-      { url: 'github.com', icon: <LinkIcon /> },
-      { url: 'github.com', icon: <GhIcon /> }
+      { url: 'https://chat-a3gg.onrender.com', icon: <LinkIcon /> },
+      { url: 'https://github.com/islf14/broadcast-server', icon: <GhIcon /> }
+    ]
+  },
+  {
+    title: 'Business Directory',
+    image: business,
+    description:
+      'This application allows you to register users, categories, and companies. It uses jsonwebtoken for authentication, bcrypt for password hashing, and blob storage for images. I also configure CSP and CORS for added security.',
+    technologies: [
+      { name: 'NodeJs', icon: <NodeIcon /> },
+      { name: 'TypeScript', icon: <TsIcon /> },
+      { name: 'NestJS', icon: <NestIcon /> },
+      { name: 'PostgreSQL', icon: <PgIcon /> },
+      { name: 'Vite', icon: <ViteIcon /> },
+      { name: 'React', icon: <ReactIcon /> },
+      { name: 'React Router', icon: <ReactRouterIcon /> },
+      { name: 'Tailwind CSS', icon: <TailwindIcon /> }
+    ],
+    links: [
+      { url: 'https://business-directory-ten.vercel.app', icon: <LinkIcon /> },
+      { url: 'https://github.com/islf14/business-nest', icon: <GhIcon /> }
     ]
   }
 ]
